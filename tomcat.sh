3a970593bac949e10c2cf787215b8aa11a92361d
nammu99 <120698706+nammu99@users.noreply.github.com>
2023-03-15 18:21:19 -0500

    Create tomcat.sh

---
+++ tomcat.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+sudo su --
+hostname tomcat-server
+amazon-linux-extras install -y java-openjdk11
+cd /opt/
+wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.7/bin/apache-tomcat-10.1.7.tar.gz
+tar -xvzf apache-tomcat-10.1.7.tar.gz 
+mv apache-tomcat-10.1.7 tomcat
+cd /opt/tomcat/bin/
+./startup.sh
